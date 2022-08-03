3486b3fb12be1202cf930597fd55845255782817
Harrison Liddiard <omniaura5@gmail.com>
2022-08-02 20:33:57 -0400

    add server-side canvas dependency

---
+++ scripts/install_cavas_deps_debian.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# canvas requires some dependencies before npm installing on Ubuntu/Debian/Raspberry Pi OS
+# https://github.com/Automattic/node-canvas#compiling
+sudo apt-get install build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
