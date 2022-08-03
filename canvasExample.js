a2f15bb5341ffb22e9f8dca479cb790e4151da04
Harrison Liddiard <omniaura5@gmail.com>
2022-08-02 20:34:19 -0400

    add canvas example

---
+++ canvasExample.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const { createCanvas, loadImage } = require('canvas')
+const canvas = createCanvas(200, 200)
+const ctx = canvas.getContext('2d')
+
+// Write "Awesome!"
+ctx.font = '30px Impact'
+ctx.rotate(0.1)
+ctx.fillText('Awesome!', 50, 100)
+
+// Draw line under text
+var text = ctx.measureText('Awesome!')
+ctx.strokeStyle = 'rgba(0,0,0,0.5)'
+ctx.beginPath()
+ctx.lineTo(50, 102)
+ctx.lineTo(50 + text.width, 102)
+ctx.stroke()
+
+// Draw cat with lime helmet
+loadImage('graphics/subway_lines/1.png').then((image) => {
+  ctx.drawImage(image, 50, 0, 70, 70)
+  const buffer = canvas.toBuffer('image/png')
+  fs.writeFileSync('./image.png', buffer)
+  console.log('<img src="' + canvas.toDataURL() + '" />')
+})
