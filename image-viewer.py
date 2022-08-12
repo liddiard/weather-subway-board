e8f9ee403216ece0e40017ef8be1b23b73b613d3
Harrison Liddiard <hliddiard@paypal.com>
2022-08-12 15:18:22 -0400

    don't show matrix while light is off

--- image-viewer.py
+++ image-viewer.py
@@ -1,12 +1,15 @@
+import asyncio
 import time
 from datetime import datetime
 
 import PIL
 from PIL import Image
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from pywizlight import wizlight
 
 
 image_file = "board.png"
+bulb_ip = "192.168.0.10"
 
 # configure matrix
 options = RGBMatrixOptions()
@@ -18,25 +21,42 @@ options.drop_privileges = False
 # options.show_refresh_rate = True
 matrix = RGBMatrix(options=options)
 
-while True:
-    canvas = matrix.CreateFrameCanvas()
-    try:
-        image = Image.open(image_file)
-    # Occasionally the image file will fail to display because it's being
-    # written by the Node.js process at the same time we are trying to read it
-    # here. A better approach would probably be to use `inotify` or OS
-    # equivalent to run this code when the file is updated, but the error
-    # happens so rarely that I'm not motivated to try another approach now.
-    # https://github.com/hzeller/rpi-rgb-led-matrix/issues/1204#issuecomment-733804488
-    except PIL.UnidentifiedImageError:
-        print(f"[{datetime.now():%Y-%m-%d %H:%M:%S%z}] Failed to open {image_file} - bad file.")
-        continue
-    image = image.convert('RGB')
 
-    canvas.SetImage(image)
-    image.close()
-    # change the matrix image without a flash to black in between
-    # https://github.com/hzeller/rpi-rgb-led-matrix/blob/master/bindings/python/README.md#performance
-    matrix.SwapOnVSync(canvas)
+async def main():
+    bulb = wizlight(bulb_ip)
+    while True:
+        bulb = wizlight(bulb_ip)
+        state = await bulb.updateState()
+        is_on = state.get_state()
 
-    time.sleep(5)
+        if not is_on:
+            matrix.Clear()
+            time.sleep(30)
+            continue
+
+        canvas = matrix.CreateFrameCanvas()
+        try:
+            image = Image.open(image_file)
+        # Occasionally the image file will fail to display because it's being
+        # written by the Node.js process at the same time we are trying to read it
+        # here. A better approach would probably be to use `inotify` or OS
+        # equivalent to run this code when the file is updated, but the error
+        # happens so rarely that I'm not motivated to try another approach now.
+        # https://github.com/hzeller/rpi-rgb-led-matrix/issues/1204#issuecomment-733804488
+        except PIL.UnidentifiedImageError:
+            print(f"[{datetime.now():%Y-%m-%d %H:%M:%S%z}] Failed to open {image_file} - bad file.")
+            continue
+        image = image.convert('RGB')
+
+        canvas.SetImage(image)
+        image.close()
+        # change the matrix image without a flash to black in between
+        # https://github.com/hzeller/rpi-rgb-led-matrix/blob/master/bindings/python/README.md#performance
+        matrix.SwapOnVSync(canvas)
+
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
