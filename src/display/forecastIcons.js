93e383d4d2af4207f93187369b3e084044d47332
Harrison Liddiard <omniaura5@gmail.com>
2022-09-20 18:03:41 -0400

    display average instead of maximum rain

--- src/display/forecastIcons.js
+++ src/display/forecastIcons.js
@@ -2,11 +2,26 @@ const { images } = require('./image')
 const { getTimeSpecificWeatherIcon } = require('./utils')
 
 
+// given an array of period descriptions and a map from description regexes to
+// values/scores, return the average score described by `descriptions` during
+// the period
+const getAverageWeather = (descriptions, descriptionToValueMap) => {
+  const maximumValue = Math.max(...Object.values(descriptionToValueMap))
+  const score = descriptions.reduce((acc, desc) => {
+    for (const [regex, value] of Object.entries(descriptionToValueMap)) {
+      if (new RegExp(regex).test(desc)) {
+        return acc + value
+      }
+    }
+    return acc
+  }, 0)
+  return score / (descriptions.length * maximumValue)
+}
+
 // given an array of forecast descriptions, return the average cloud cover
 // mentioned by them on a scale from 0 (clear the whole time) to 4 (overcast
 // the whole time)
 const getAverageCloudCover = (descriptions) => {
-  // description (exact match) to "score"
   const descToScore = {
     'Mostly Sunny': 1,
     'Mostly Clear': 1,
@@ -16,29 +31,19 @@ const getAverageCloudCover = (descriptions) => {
     'Mostly Cloudy': 3,
     'Overcast': 4
   }
-  const cloudScore = descriptions.reduce((acc, cur) => 
-    acc += descToScore[cur] || 0, 0)
-  return cloudScore / (descriptions.length * descToScore.Overcast)
+  return getAverageWeather(descriptions, descToScore)
 }
 
-// get the maximum rain mentioned in forecast descriptions from 0 (no rain) to
+// get the average rain mentioned in forecast descriptions from 0 (no rain) to
 // 3 (heavy rain)
-const getMaximumRain = (descriptions) => {
-  // description regex to intensity score
+const getAverageRain = (descriptions) => {
   const descToScore = {
     'Light Rain': 1,
     'Showers': 2,
     '^Rain$': 2,
     'Heavy Rain': 3
   }
-  return descriptions.reduce((acc, cur) => {
-    for (const [regex, value] of Object.entries(descToScore)) {
-      if (new RegExp(regex).test(cur) && value > acc) {
-        return value
-      }
-    }
-    return acc
-  }, 0)
+  return getAverageWeather(descriptions, descToScore)
 }
 
 // given an array of forecast weather periods, summarize all periods based on
@@ -58,7 +63,7 @@ const summarizeWeatherPeriods = (periods) => {
     fog: descriptions.some(d =>
       d.includes('Fog')),
     // most intense rain during the periods, scale from 1 to 3
-    rain: getMaximumRain(descriptions),
+    rain: getAverageRain(descriptions),
     // whether thunderstorms are possible during ANY period
     thunderstorms: descriptions.some(d =>
       d.includes('Thunderstorms')),
@@ -76,24 +81,31 @@ const getCloudIcon = (cloudCover) => {
   if (cloudCover < 0.2) {
     return null
   }
-  if (cloudCover >= 0.2 && cloudCover < 0.4) {
+  if (cloudCover < 0.4) {
     return 'cloud'
   }
-  if (cloudCover >= 0.4 && cloudCover < 0.6) {
+  if (cloudCover < 0.6) {
     return 'two_clouds'
   }
-  if (cloudCover >= 0.6 && cloudCover < 0.8) {
+  if (cloudCover < 0.8) {
     return 'broken_clouds'
   }
   return 'overcast'
 }
 
-// return the rain icon associated with the rain amount integer provided
-const getRainIcon = (rainAmount) => ({
-    1: 'sprinkles',
-    2: 'heavy_rain',
-    3: 'downpour'
-  }[rainAmount])
+// return the rain icon matching a given rain "score" from 0 to 1
+const getRainIcon = (rainAmount) => {
+  if (rainAmount === 0) {
+    return null
+  }
+  if (rainAmount < 0.33) {
+    return 'sprinkles'
+  }
+  if (rainAmount < 0.67) {
+    return 'heavy_rain'
+  }
+  return 'downpour'
+}
 
 // given a forecast weather summary and an x/y offset, draw a composite weather
 // icon
