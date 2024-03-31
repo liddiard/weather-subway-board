a2e5648ccde81eabccb3aa2f2f958c094a47f045
Harrison Liddiard <omniaura5@gmail.com>
2024-03-31 19:34:28 -0400

    add weather mapping for 'Clear and Windy'

--- src/constants.js
+++ src/constants.js
@@ -100,7 +100,8 @@ const constants = {
     'Heavy Snow and Fog': 'fog_with_blizzard',
     'Heavy Snow and Fog/Mist': 'fog_with_blizzard',
     'Light Freezing Rain and Fog/Mist': 'fog_with_mixed',
-    'Freezing Rain and Fog/Mist': 'fog_with_mixed'
+    'Freezing Rain and Fog/Mist': 'fog_with_mixed',
+    'Clear and Windy': 'wind'
   },
   // how "warm" to make the display colors when the sun is down, from 0 (not at
   // all) to 1 (white text appears fully orange)
